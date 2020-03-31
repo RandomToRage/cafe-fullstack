37607e665bdce3257e7225a7fdbbe08571c7a586
Damien Davis <xamdgoku@gmail.com>
2020-03-31 01:49:36 -0500

    Made template layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,115 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.landing-page')
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="/css/app.css">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-    <section id="welcome">
-      <div class="welcome-menu">
-        <div class="logo">
-            <img src="https://dlp2gfjvaz867.cloudfront.net/product_photos/61526322/file_d56099cedf_original.png">
-        </div>
-        <div class="menu">
-          <div class="menu-title">
-            Menu
-          </div>
-          <ul class="links">
-            <li>
-              <a href="#">Menu</a>
-            </li>
-            <li>
-                <a href="#">Waitlist</a>
-              </li>
-              <li>
-                  <a href="#">Giftcards</a>
-                </li>
-                <li>
-                    <a href="#">Offers</a>
-                  </li>
-          </ul>
-        </div>
-        <div class="social-icons">
-          <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-        </div>
-        <div class="location">
-          <div class="address">
-              3121 hansell st,<br>
-              Fort Worth, Texas 76117
-          </div>
-          <div class="phone-number">
-            <a href="tel:(817)949-3636">(817) 949-3636</a>
-          </div>
-        </div>
-      </div>
-      <div class="welcome-jumbo">
-        <div class="status">New</div>
-        <h1>Noir Blend</h1>
-        <img src="https://www.pngkey.com/png/full/800-8004133_kahla-cold-brew-black-russian.png" class="coffee-jumbo">
-      </div>
-    </section>
+@section('content')
     <section id="information">
       <div class="cafe-image">
         <img src="img/views1.jpg">
@@ -257,5 +149,4 @@
       </div>
       
     </section>
-    </body>
-</html>
+@endsection
---
+++ resources/views/includes/side-menu.blade.php
@@ -0,0 +1,38 @@
+<div class="welcome-menu">
+        <a href="/" class="logo">
+            <img src="https://dlp2gfjvaz867.cloudfront.net/product_photos/61526322/file_d56099cedf_original.png">
+        </a>
+        <div class="menu">
+          <div class="menu-title">
+            Menu
+          </div>
+          <ul class="links">
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+                <a href="/waitlist">Waitlist</a>
+              </li>
+              <li>
+                  <a href="/giftcards">Giftcards</a>
+                </li>
+                <li>
+                    <a href="/offers">Offers</a>
+                  </li>
+          </ul>
+        </div>
+        <div class="social-icons">
+          <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+        <div class="location">
+          <div class="address">
+              3121 hansell st,<br>
+              Fort Worth, Texas 76117
+          </div>
+          <div class="phone-number">
+            <a href="tel:(817)949-3636">(817) 949-3636</a>
+          </div>
+        </div>
+      </div>
