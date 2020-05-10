ba6b29f0de2d25fe648198ad515e1bae302506c8
Damien Davis <xamdgoku@gmail.com>
2020-05-10 08:41:09 -0500

    created food items admin pages

--- resources/views/admin/food-items/create.blade.php
+++ resources/views/admin/food-items/create.blade.php
@@ -11,14 +11,14 @@
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="page-header">
-                            <h2 class="pageheader-title">Edit Food Category </h2>
+                            <h2 class="pageheader-title">New Food Items </h2>
                             <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                             <div class="page-breadcrumb">
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-                                        <li class="breadcrumb-item"><a href="/admin/food-categories" class="breadcrumb-link">All Food Categories</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">Edit Food Category</li>
+                                        <li class="breadcrumb-item"><a href="/admin/food-items" class="breadcrumb-link">All Food Items</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">New Food Item</li>
                                     </ol>
                                 </nav>
                             </div>
@@ -36,25 +36,24 @@
                         <!-- ============================================================== -->
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="card">
-                                <h5 class="card-header">Edit Category</h5>
+                                <h5 class="card-header">Create New Item</h5>
                                 <div class="card-body">
                                     <form action="#" id="basicform" data-parsley-validate="" novalidate="">
                                         <div class="form-group">
-                                            <label for="inputCategory">Category Name</label>
-                                            <input id="inputCategory" type="text" name="category" data-parsley-trigger="change" required="" placeholder="Enter Category Name" autocomplete="off" class="form-control">
+                                            <label for="inputItem">Item Name</label>
+                                            <input id="inputItem" type="text" name="title" data-parsley-trigger="change" required="" placeholder="Enter Item Name" autocomplete="off" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputItem">Price</label>
+                                            <input id="inputItem" type="text" name="price" data-parsley-trigger="change" required="" placeholder="Enter Item price" autocomplete="off" class="form-control">
                                         </div>
                                         <div class="row">
                                             <div class="col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
                                             </div>
-                                            <div class="col-sm-6 pl-0">
-                                                <p class="text-right">
-                                                    <button type="submit" class="btn btn-space btn-primary">Submit</button>
-                                                </p>
-                                            </div>
                                         </div>
                                         <div class="form-group">
-                                            <label for="inputCategoryImageUrl">Category Image Url/File upload</label>
-                                            <input id="inputCategoryImageUrl" type="text" name="image_url" data-parsley-trigger="change" required="" placeholder="http://www.8bitcafe.com/img/coffee.jpg" autocomplete="off" class="form-control">
+                                            <label for="inputItemImageUrl">Category Image Url/File upload</label>
+                                            <input id="inputItemImageUrl" type="text" name="image_url" data-parsley-trigger="change" required="" placeholder="http://www.8bitcafe.com/img/coffee.jpg" autocomplete="off" class="form-control">
                                         </div>
                                         <div class="row">
                                             <div class="col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
