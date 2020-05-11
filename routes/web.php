ee1d0d2988de27da902bb00f16fcbe75037cbf18
Damien Davis <xamdgoku@gmail.com>
2020-05-11 14:47:31 -0500

    Created users pages

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,11 @@ Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers'
 Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
 Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
 
+//Admin Users
+Route::get('/admin/users', 'admin\UsersController@index');
+Route::get('/admin/users/create', 'admin\UsersController@create');
+Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
+
 //Admin Authentication
 Route::get('/admin/register', function () {
     return view('admin/register');
