17406c724c7c13d71150f93b7b3a875a43dc9485
Damien Davis <xamdgoku@gmail.com>
2020-11-11 02:17:09 -0600

    created new member for front page signup

--- app/Http/Controllers/admin/MemberController.php
+++ app/Http/Controllers/admin/MemberController.php
@@ -35,7 +35,7 @@ class MemberController extends Controller
         $member->image_url = request('image_url');
         $member->save();
 
-
+        return "Thank you";
         return redirect('/admin/food-members');
 }
     public function edit($id){
