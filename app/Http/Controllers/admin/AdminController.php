d2acebcbc57226b3d489accc1da6c49f5e14186d
Damien Davis <xamdgoku@gmail.com>
2020-11-23 20:59:09 -0600

    adding data to be recognized on dashboard.  Must go back to add graph resposivness to data

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use App\Member;
+use App\Reservation;
+use DB;
 
 class AdminController extends Controller
 {
@@ -12,6 +16,41 @@ class AdminController extends Controller
         $this->middleware('auth');
     }
     public function dashboard(){
-        return view('admin/dashboard');
+        
+        $estimated_income_last_30 = DB::select(DB::raw('
+        SELECT 
+            (sum(guests_total) * 13) as total
+        FROM cafe.reservations
+        WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()'));
+
+        $estimated_income_last_30 = $estimated_income_last_30;
+
+        $total_customers_last_30 = DB::select(DB::raw('
+        SELECT 
+            sum(guests_total) as total
+        FROM cafe.reservations
+        WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()'));
+
+        $total_reservations_last_30 = DB::select(DB::raw('
+        SELECT 
+            count(*) as total
+        FROM cafe.reservations
+        WHERE created_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()'));
+
+        $total_employees_last_30 = DB::select(DB::raw('
+        SELECT  count(*) as total
+            from users
+            inner join role_user on role_user.user_id = users.id
+            inner join roles on roles.id = role_user.role_id
+            where role_id = 2'));
+
+
+
+        return view('admin/dashboard', [
+            "estimated_income_last_30" => "$".$estimated_income_last_30[0]->total,
+            "total_customers_last_30" => $total_customers_last_30[0]->total,
+            "total_reservations_last_30" => $total_reservations_last_30[0]->total,
+            "total_employees_last_30" => $total_employees_last_30[0]->total
+        ]);
     }
 }
