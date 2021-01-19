ac6d420cfa04fad0ef9610f0291c1218dbbbc296
Damien Davis <xamdgoku@gmail.com>
2021-01-19 17:24:33 -0600

    git

--- app/Http/Controllers/admin/MemberController.php
+++ app/Http/Controllers/admin/MemberController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Member;
+use App\Models\Member;
 
 class MemberController extends Controller
 {
